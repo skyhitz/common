bf18a5102816baa93d621cfe782e78f8995c62a1
Alejo <alejandro@skyhitzmusic.com>
2017-10-07 11:53:13 -0500

    update

--- src/stores/index.ts
+++ src/stores/index.ts
@@ -1,7 +1,9 @@
+import { PlayerStore } from './player.store';
 import { InputSearchStore } from './input-search.store';
 import { EntriesSearchStore } from './entries-search.store';
 import { UsersSearchStore } from './users-search.store';
 
+export const playerStore = new PlayerStore();
 export const inputSearchStore = new InputSearchStore();
 export const entriesSearchStore = new EntriesSearchStore(inputSearchStore.query);
 export const usersSearchStore = new UsersSearchStore(inputSearchStore.query);
