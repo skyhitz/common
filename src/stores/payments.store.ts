d252ee3f12e51a7b4486079e7af545155acfdfb4
alejo <alejandro@skyhitzmusic.com>
2018-08-27 16:44:42 -0500

    add loading sub

--- src/stores/payments.store.ts
+++ src/stores/payments.store.ts
@@ -4,6 +4,8 @@ import { paymentsBackend } from '../backends/payments.backend';
 export class PaymentsStore {
   @observable
   subscribed: boolean = false;
+  @observable
+  loadingSubscription: boolean = false;
   constructor() {}
 
   async subscribeUser(cardToken: string) {
@@ -13,7 +15,9 @@ export class PaymentsStore {
   }
 
   async refreshSubscription() {
+    this.loadingSubscription = true;
     let subscribed = await paymentsBackend.refreshSubscription();
     this.subscribed = subscribed;
+    this.loadingSubscription = false;
   }
 }
