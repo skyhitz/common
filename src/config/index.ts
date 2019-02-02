97ea2681a3d2ec179b7b99b814a6f0664aaeaf41
alejo <alejo@splice.com>
2019-02-01 20:52:06 -0500

    take out unnecessary staging config

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,14 +1,7 @@
 import { ConfigInterface } from './config';
 import { ProductionConfig } from './config.production';
-import { StagingConfig } from './config.staging';
 
 const productionModeOn = process.env.NODE_ENV === 'production';
-function getConfig(): ConfigInterface {
-  if (productionModeOn) {
-    return ProductionConfig;
-  }
-  return StagingConfig;
-}
 
-export const Config: ConfigInterface = getConfig();
+export const Config: ConfigInterface = ProductionConfig;
 export const isTesting = productionModeOn ? 0 : 1;
