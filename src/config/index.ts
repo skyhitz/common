c1bfd35c306d700580d46edaf340f14035b26284
alejo <alejo@splice.com>
2019-02-06 18:31:23 -0500

    update env

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,7 +1,7 @@
 import { ConfigInterface } from './config';
 import { ProductionConfig } from './config.production';
 
-const productionModeOn = process.env.NODE_ENV === 'production';
+const productionModeOn = process.env.SKYHITZ_ENV === 'production';
 
 export const Config: ConfigInterface = ProductionConfig;
 export const isTesting = productionModeOn ? 0 : 1;
