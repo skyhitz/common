0daa8b939f5b1123da923bff54c3e84ff876f2da
alejo <alejo@splice.com>
2019-02-17 20:44:01 -0500

    support sending credits

--- src/stores/payments.store.ts
+++ src/stores/payments.store.ts
@@ -1,14 +1,22 @@
-import { observable } from 'mobx';
+import { observable, observe, IObservableObject } from 'mobx';
 import { paymentsBackend } from '../backends/payments.backend';
+import { Set, List } from 'immutable';
+import { Entry, User } from '../models';
 
 export class PaymentsStore {
+  @observable ids: Set<string> = Set([]);
   @observable
   subscribed: boolean = false;
   @observable
   subscriptionLoaded: boolean = false;
   @observable
   credits: number = 0;
-  constructor() {}
+  counter: number = 0;
+  timeout: any;
+  @observable userFavorites: List<Entry> = List([]);
+  @observable entry: Entry;
+
+  constructor(public observables: IObservableObject) {}
 
   async subscribeUser(cardToken: string) {
     await paymentsBackend.subscribe(cardToken);
@@ -22,4 +30,48 @@ export class PaymentsStore {
     this.credits = credits;
     this.subscriptionLoaded = true;
   }
+
+  async sendCredits() {
+    console.log('just sent credits', this.counter);
+    clearTimeout(this.timeout);
+    this.timeout = null;
+    this.counter = 0;
+  }
+
+  async sendCredit(entry: Entry) {
+    this.counter++;
+    if (!this.timeout) {
+      this.timeout = setTimeout(this.sendCredits, 1000);
+    }
+
+    if (this.ids.has(entry.id)) {
+      return;
+    }
+    this.ids = this.ids.add(entry.id);
+    this.userFavorites = this.userFavorites.push(entry);
+  }
+
+  get isFavorited() {
+    if (!this.entry) {
+      return false;
+    }
+    return this.ids.has(this.entry.id);
+  }
+
+  public disposer = observe(this.observables, ({ object }) => {
+    if (!object.entry) {
+      return;
+    }
+    this.entry = object.entry;
+  });
+
+  // public refreshFavorites() {
+  //   likesBackend.userFavorites()
+  //     .then(userFavorites => {
+  //       let ids = userFavorites.map((favorite: any) => favorite.id);
+  //       let entries = userFavorites.map((favorite: any) => new Entry(favorite));
+  //       this.ids = Set(ids);
+  //       this.userFavorites = List(entries);
+  //     });
+  // }
 }
