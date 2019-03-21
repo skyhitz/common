a14041d445f091f3375d7e46ed995158889a1989
alejo <alejo@splice.com>
2019-03-20 22:21:09 -0500

    update config

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,7 +1,14 @@
 import { ConfigInterface } from './config';
 import { ProductionConfig } from './config.production';
+import { StagingConfig } from './config.staging';
 
 const productionModeOn = process.env.SKYHITZ_ENV === 'production';
-
-export const Config: ConfigInterface = ProductionConfig;
 export const isTesting = productionModeOn ? 0 : 1;
+
+let config: ConfigInterface = ProductionConfig;
+
+if (isTesting) {
+  config = StagingConfig;
+}
+
+export const Config = config;
