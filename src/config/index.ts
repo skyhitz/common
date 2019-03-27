99ebae101dea275d0546c9d2d24bf0034ecf3059
alejo <alejo@splice.com>
2019-03-27 11:30:50 -0500

    add dev to support production mode

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,8 +1,9 @@
 import { ConfigInterface } from './config';
 import { ProductionConfig } from './config.production';
 import { StagingConfig } from './config.staging';
+declare const __DEV__: boolean;
 
-const productionModeOn = process.env.SKYHITZ_ENV === 'production';
+const productionModeOn = process.env.SKYHITZ_ENV === 'production' || !__DEV__;
 export const isTesting = productionModeOn ? 0 : 1;
 
 let config: ConfigInterface = ProductionConfig;
