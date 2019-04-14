ad2d5e8dbcfe3155e3881246ac928f71e7808619
alejo <alejo@splice.com>
2019-04-14 16:52:57 -0500

    update testing mode

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,10 +1,9 @@
 import { ConfigInterface } from './config';
 import { ProductionConfig } from './config.production';
 import { StagingConfig } from './config.staging';
-declare const __DEV__: boolean;
 
-const productionModeOn = process.env.SKYHITZ_ENV === 'production' || !__DEV__;
-export const isTesting = productionModeOn ? 0 : 1;
+const testingModeOn = process.env.SKYHITZ_ENV === 'staging';
+export const isTesting = testingModeOn ? 1 : 0;
 
 let config: ConfigInterface = ProductionConfig;
 
